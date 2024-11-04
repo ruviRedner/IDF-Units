e34abecbd34e9dba945647c90999d8ad048e9901
ruviRedner <r.y.r7966@gmail.com>
2024-11-04 18:33:15 +0200

    done

--- src/compo/UnitStatus.tsx
+++ src/compo/UnitStatus.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
-
-const UnitStatus = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import React, { useContext } from "react";
+import { DeploymentContext } from "../context/DeploymentContext";
+interface Props {
+  unitname: string;
 }
+const UnitStatus = ({ unitname }: Props) => {
+  const deploymentContext = useContext(DeploymentContext);
+  if (!deploymentContext) {
+    throw new Error("UnitStatus must be used within a DeploymentProvider");
+  }
+  const { units } = deploymentContext;
+  return <div>
+    <p>{unitname}:{units[unitname]} </p>
+  </div>;
+}; 
 
-export default UnitStatus
+export default UnitStatus;
