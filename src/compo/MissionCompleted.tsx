f0c94d13c26ee35a8338844c367f1f1d3bd8b6a4
ruviRedner <r.y.r7966@gmail.com>
2024-11-04 18:32:53 +0200

    done

--- src/compo/MissionCompleted.tsx
+++ src/compo/MissionCompleted.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { DeploymentContext } from "../context/DeploymentContext";
 
 const MissionCompleted = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const deploymentContext = useContext(DeploymentContext);
+  if (!deploymentContext) {
+    throw new Error(
+      "MissionCompleted must be used within a DeploymentProvider"
+    );
+  }
 
-export default MissionCompleted
+  const { units } = deploymentContext;
+  console.log(units);
+  const allcompleted = Object.values(units).every(
+    (status) => status === "Completed"
+  );
+
+  return <div>{allcompleted && <h2>כל היחידות הושלמו בהצלחה</h2>}</div>;
+};
+export default MissionCompleted;
