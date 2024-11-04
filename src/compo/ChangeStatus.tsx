b2865b117b25e7da70002ff4384989af9b27e8ca
ruviRedner <r.y.r7966@gmail.com>
2024-11-04 18:32:42 +0200

    done

--- src/compo/ChangeStatus.tsx
+++ src/compo/ChangeStatus.tsx
@@ -1,11 +1,48 @@
-import React from 'react'
-
-const ChangeStatus = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import React, { useContext, useState } from "react";
+import { DeploymentContext } from "../context/DeploymentContext";
+interface Props {
+  unitname: string;
+}
+enum status {
+  idel = "Idle",
+  Deployed = "Deployed",
+  Active = "Active",
+  Completed = "Completed",
 }
 
-export default ChangeStatus
+const ChangeStatus: React.FC<Props> = ({ unitname }: Props) => {
+  const deploymentContext = useContext(DeploymentContext);
+  if (!deploymentContext) {
+    throw new Error("UnitStatus must be used within a DeploymentProvider");
+  }
+
+  const { setUnitStatus, units } = deploymentContext;
+  const cuurentStatus = units[unitname];
+  const hendelStatus = () => {
+    let nextStatus;
+    switch (cuurentStatus) {
+      case status.idel:
+        nextStatus = status.Deployed;
+        break;
+
+      case status.Deployed:
+        nextStatus = status.Active;
+        break;
+      case status.Active:
+        nextStatus = status.Completed;
+        break;
+
+      default:
+        throw new Error("Invalid status");
+    }
+    setUnitStatus(unitname, nextStatus);
+  };
+
+  return (
+    <div>
+      <button onClick={hendelStatus}>{unitname} פריסת</button>
+    </div>
+  );
+};
+
+export default ChangeStatus;
