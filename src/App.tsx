b3c54d1cb2b88bad7fec4b1457b902380aa2c782
ruviRedner <r.y.r7966@gmail.com>
2024-11-04 18:32:13 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useContext } from "react";
+import UnitList from "./compo/UnitList";
+import MissionCompleted from "../src/compo/MissionCompleted";
+import { DeploymentContext } from "./context/DeploymentContext";
 
-const App = () => {
+const App: React.FC = () => {
+  const deploymentContext = useContext(DeploymentContext);
+  if (!deploymentContext) {
+    throw new Error("UnitList must be used within a DeploymentProvider");
+  }
+  const { units, setUnitStatus } = deploymentContext;
+  const hendelClick = () => {
+    Object.keys(units).forEach((unitname) => {
+      setUnitStatus(unitname, "Idel");
+    });
+  };
   return (
-    <div>
-      
+    <div
+      style={{
+        padding: "20px",
+        fontFamily: "Arial, sans-serif",
+        textAlign: "center",
+      }}
+    >
+      <h1>מעקב פריסת יחידות צה"ל</h1>
+      <UnitList />
+      <MissionCompleted />
+      <button onClick={hendelClick}>רענון</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
