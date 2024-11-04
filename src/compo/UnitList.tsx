468627262f5c0bc0e947ce29d49257227626eecc
ruviRedner <r.y.r7966@gmail.com>
2024-11-04 18:33:05 +0200

    done

--- src/compo/UnitList.tsx
+++ src/compo/UnitList.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { DeploymentContext } from "../context/DeploymentContext";
+import UnitStatus from "./UnitStatus";
+import ChangeStatus from "./ChangeStatus";
 
 const UnitList = () => {
+  const deploymentContext = useContext(DeploymentContext);
+  if (!deploymentContext) {
+    throw new Error("UnitList must be used within a DeploymentProvider");
+  }
+  const { units } = deploymentContext;;
+
   return (
     <div>
-      
+      <h2>רשימת יחידות</h2>
+      {Object.keys(units).map((unitname) => (
+        <div key={unitname}>
+        <UnitStatus unitname={unitname}  />
+        <ChangeStatus unitname={unitname}  />
+        </div>
+      ))}
+        
     </div>
-  )
-}
+  );
+};
 
-export default UnitList
+export default UnitList;
