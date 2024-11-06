4ffa2d8b4e1f8b32e0794c2b0534deb0092bcb5d
ruviRedner <r.y.r7966@gmail.com>
2024-11-06 19:31:45 +0200

    done

--- src/context/DeploymentContext.tsx
+++ src/context/DeploymentContext.tsx
@@ -1,19 +1,25 @@
 import { createContext, useState } from "react";
 import DeploymentContextType from "../types/deployment";
 import Children from "../types/children";
-const DeploymentContext = createContext<DeploymentContextType | undefined>(undefined)
-const DeploymentProvider: React.FC<Children> = ({ children }:Children) => {
-   
-    const [units, setUnits] = useState({ Golani: 'Idle', Paratroopers: 'Idle',
-    Givati: 'Idle' ,SeaArm:"Idle",Artillery: 'Idle',});
+const DeploymentContext = createContext<DeploymentContextType | undefined>(
+  undefined
+);
+const DeploymentProvider: React.FC<Children> = ({ children }: Children) => {
+  const [units, setUnits] = useState({
+    Golani: "Idle",
+    Paratroopers: "Idle",
+    Givati: "Idle",
+    SeaArm: "Idle",
+    Artillery: "Idle",
+  });
 
-    const setUnitStatus = (unit: string, status: string) => {
-        setUnits((prevUnits) => ({...prevUnits, [unit]: status }));
-    };
-    return (
-        <DeploymentContext.Provider value={{ units, setUnitStatus }}>
-            {children}
-        </DeploymentContext.Provider>
-    );
-}
-export {DeploymentProvider,DeploymentContext}
+  const setUnitStatus = (unit: string, status: string) => {
+    setUnits((prevUnits) => ({ ...prevUnits, [unit]: status }));
+  };
+  return (
+    <DeploymentContext.Provider value={{ units, setUnitStatus }}>
+      {children}
+    </DeploymentContext.Provider>
+  );
+};
+export { DeploymentProvider, DeploymentContext };
